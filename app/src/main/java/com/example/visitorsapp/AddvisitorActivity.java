b842f3f0a729fab541b35efea7b9879db5b0e7d7
PunnyaR <punnyaramesh543@gmail.com>
2024-12-19 10:32:49 +0530

    completed

--- app/src/main/java/com/example/visitorsapp/AddvisitorActivity.java
+++ app/src/main/java/com/example/visitorsapp/AddvisitorActivity.java
@@ -1,16 +1,24 @@
 package com.example.visitorsapp;
 
+import android.app.DownloadManager;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.PixelCopy;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.reflect.Method;
 
 public class AddvisitorActivity extends AppCompatActivity {
 
@@ -31,13 +39,46 @@ public class AddvisitorActivity extends AppCompatActivity {
         b2=(Button) findViewById(R.id.bm);
 
         b1.setOnClickListener(new View.OnClickListener() {
+            class JsonObjectrequest {
+            }
+
             @Override
             public void onClick(View v) {
                 getFn=e1.getText().toString();
                 getLn=e2.getText().toString();
                 getP=e3.getText().toString();
                 getW=e4.getText().toString();
+                if (getFn.isEmpty()||getLn.isEmpty()||getP.isEmpty()||getW.isEmpty())
+                    Toast.makeText(getApplicationContext(),)
+                else
+                    callApi()
+
+
+                }
+                void callApi() throws JSONException {
+                String apiUrl="https://log-app-demo-api.onrender.com/addvisitor";
+                JSONObject data=new JSONObject();
+                try {
+                    data.put("firstname",getFn);
+                    data.put("lastname",getLn);
+                    data.put("purpose",getP);
+                    data.put("whomToMeet",getW);
+
+                } catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+                JsonObjectRequest=new JsonObjectRequest(
+                        Request.Method.POST,
+                        apiUrl,
+                        data,
+                        response -> Toast.makeText(getApplicationContext(),"suuccessfully added",Toast.LENGTH_LONG).show(),
+                        error ->  Toast.makeText(getApplicationContext(),"something went wrong",Toast.LENGTH_LONG).show(),
+                );
+                RequestQueue queue=Volley.newRequestQueue(getApplicationContext());
+
+
             }
+        }
         });
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
